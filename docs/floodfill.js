f005424b6f2be250c529f35dd0ea48b296140dc1
michiwo <mk1104kado@gmail.com>
2024-10-05 09:30:46 -0700

    Remove unused variable 'ctx'

--- docs/floodfill.js
+++ docs/floodfill.js
@@ -7,7 +7,6 @@
 
         // Canvas references
         const canvas = document.querySelector("canvas");
-        const ctx = canvas.getContext("2d");
 
         // UI references
         const restartButton = document.querySelector("#restart");
